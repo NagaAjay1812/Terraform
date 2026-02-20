53fab6958ff250f20fea88ffc849ed774ffe013b
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-02-20 13:02:10 -0500

    actions

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -6,5 +6,36 @@ resource "aws_instance" "roboshop_instance" {
   tags = {
     Name    = "terraform"
     Project = "Roboshop"
+
+
+    
+  }
+}
+
+
+resource "aws_security_group" "Security_groups" {
+  name        = "terraform_testing_sg" # this is for AWS account
+  description = "Allow TLS inbound traffic"
+  
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  
+  }
+
+  tags = {
+    Name = "terraform_testing_sg"
   }
 }
