82d022ef55bcb33b728b8894271720137949930a
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-02-24 16:43:53 -0500

    Cleanup: remove tfstate from repo and keep folder structure

--- variables.tf
+++ variables.tf
@@ -1,14 +1,12 @@
 variable "instances" {
-  type    = list(any)
+  type    = list
   default = ["mongodb", "catalogue", "redis", "user", "cart", "mysql", "shipping", "rabbitmq", "payment", "frontend"]
 }
 
-variable "zone_id" {
-  default = "Z07326442Z8C3IRLJ3030"
+variable "zone_id"{
+    default = "Z07326442Z8C3IRLJ3030"
 }
 
-variable "domain_name" {
-  default = "cloudkarna.in"
+variable "domain_name"{
+    default = "cloudkarna.in"
 }
-
-
